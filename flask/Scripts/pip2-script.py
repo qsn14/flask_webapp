65b082744f43b4f9a09725f85ed67cf94f555ddf
Stephanie <engsm@uci.edu>
2014-05-24 19:12:29 -0700

    has text hover now

---
+++ flask/Scripts/pip2-script.py
@@ -0,0 +1,9 @@
+#!c:\Users\potatos\github\flask_webapp\flask\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==1.5.6','console_scripts','pip2'
+__requires__ = 'pip==1.5.6'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('pip==1.5.6', 'console_scripts', 'pip2')()
+)
